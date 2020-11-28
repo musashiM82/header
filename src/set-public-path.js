bd0570fa8229970aa63c4b211b0164b89b70ad5a
peppe82 <>
2020-11-28 14:23:13 +0100

    first commit

---
+++ src/set-public-path.js
@@ -0,0 +1,3 @@
+import { setPublicPath } from "systemjs-webpack-interop";
+
+setPublicPath("@example/header", 2);
---
+++ vue.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const pkjson = require("./package.json");
+const webpack = require("webpack");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
+
+const isProduction = process.env.NODE_ENV === "production";
+const fileVersion = isProduction ? "." + pkjson.version : "";
+
+module.exports = {
+  configureWebpack: {
+    output: {
+      filename: "js/[name]" + fileVersion + ".js",
+      chunkFilename: "[name]" + fileVersion + ".js",
+      library: "@example/header",
+      libraryTarget: "system"
+    },
+    externals: ['vue', 'vue-router'],
+    plugins: [
+      new BundleAnalyzerPlugin({
+        analyzerMode: "static",
+        openAnalyzer: false,
+        reportFilename: "./report.html"
+      })
+    ]
+  },
+  chainWebpack: config => {
+    config.plugins.delete("prefetch");
+  }
+};
